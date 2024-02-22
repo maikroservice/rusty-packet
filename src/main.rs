58e9eb243da0f5d15e1d3885132a6269319809ac
Maik Ro <20245897+maikroservice@users.noreply.github.com>
2024-02-23 00:22:28 +0100

    kinda works

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use ascii::AsciiString;
 use clap::Parser;
 use kerbeiros::*;
 use kerberos_ccache::CCache;
+use ldap3::SearchEntry;
+use ldap3::SearchResult;
 use std::fs;
 use std::net::*;
 use std::path::PathBuf;
 use std::vec::Vec;
+use ldap3::{LdapConn, Scope};
 
 #[derive(Parser)]
 struct Cli {
@@ -56,7 +59,7 @@ fn main() {
                     .split('.')
                     .map(|octet_str: &str| octet_str.parse().unwrap()).collect();     
                    
-    let kdc_address = Some(IpAddr::V4(Ipv4Addr::new(dc_ip[0], dc_ip[1], dc_ip[2], dc_ip[3])));
+    let kdc_address = Some(IpAddr::V4(kdc_ip.parse::<Ipv4Addr>().unwrap()));// Ipv4Addr::new(kdc_ip.parse()::Ipv4Addr)() //dc_ip[0], dc_ip[1], dc_ip[2], dc_ip[3])));
 
     // request TGT
     let tgt_requester = TgtRequester::new(realm.unwrap(), kdc_address.unwrap());
@@ -74,4 +77,52 @@ fn main() {
         .1;
 
     println!("ticket saved and loaded");
+
+    fn find_spn_accounts(username: &str) {//username: &str, password: &str, dc_ip: Option<String>, domain: Option<String>, use_ssl: bool) {
+        /*let ldap_host = dc_ip.unwrap();
+        let ldap_port: u16 = match use_ssl {
+            true => 636,
+            false => 389,
+            _ => 389,
+        };
+        let ldap_pass = password;
+        */
+        let ldap =LdapConn::new("ldap://192.168.0.8:389");
+
+        let mut ldapcon = match ldap{
+         Ok(l) => l,
+         Err(r) => panic!("{}",r)
+        };
+        
+        let displayname =  username;
+        // can we do this without binding?
+        ldapcon.simple_bind("", "").unwrap();
+
+        let filter = format!("(&(objectClass=user)(displayName={}))", displayname);
+        println!("filter: {}",filter);   
+        let res = ldapcon.search("DC=snackempire,DC=home",Scope::Subtree,&filter[..],vec!["dn"]).unwrap();     
+        let (re, _ldapresult) = res.success().unwrap();
+        let mut bind_dn: String = String::new();
+        for i in re { 
+            bind_dn = SearchEntry::construct(i).dn;
+           }
+
+        ldapcon.simple_bind(&bind_dn, "Password123!").unwrap();
+     //"CN=maik ro,CN=Users,DC=snackempire,DC=home"
+        let filter = "(&(objectClass=user)(servicePrincipalName=*)(!(objectCategory=computer))(!(cn=krbtgt)))";
+        //let filter = format!("(&(objectClass=user)(displayName={}))", displayname);
+
+
+        //let filter = "(objectclass=user)";
+        println!("filter: {}",filter);
+        let res = ldapcon.search("DC=snackempire,DC=home",Scope::Subtree,&filter[..],vec!["dn"]).unwrap();
+     
+        let (re, _ldapresult) = res.success().unwrap();
+     
+        for i in re{
+         println!("{:#?}",SearchEntry::construct(i).dn);
+        }
+    }
+
+    find_spn_accounts(&username.as_str());
 }
